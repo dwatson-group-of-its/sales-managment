c0ca16dc6faef0da8d42bb8ea71e3af5c450548d
dwatson-group-of-its <onlydevsx@gmail.com>
2025-12-02 22:49:03 +0500

    Update cors.js

--- server/config/cors.js
+++ server/config/cors.js
@@ -6,6 +6,7 @@ export const corsOptions = {
     }
     const allowedOrigins = [
       'https://www.dwatson.online',
+      'http://www.dwatson.online',
       'https://multifiles-testing-7a8c586ca438.herokuapp.com',
       'https://shopmodule-9c27ee49e041.herokuapp.com',
       'https://dwatson-db-902c7d197f9e.herokuapp.com',
